10a6ce56a86b2dcb756fa32227a62e1dcdf90be4
rohannrk <rohanrk39357@gmail.com>
2023-09-29 23:46:29 +0530

    auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,36 @@
-import { Button } from "@/components/ui/button";
+import MaxWidthWrapper from '@/components/MaxWidthWrapper'
+import Link from 'next/link'
+import { ArrowRight } from 'lucide-react'
+import { buttonVariants } from '@/components/ui/button'
+import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div>
-      <Button>
-        Click me
-      </Button>
-    </div>
+    <>
+      <MaxWidthWrapper className='mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center'>
+        <div className='mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50'>
+          <p className='text-sm font-semibold text-gray-700'>
+            Evently is now public!
+          </p>
+        </div>
+        <h1 className='max-w-5xl text-5xl font-bold md:text-6xl lg:text-7xl'>
+          Simplifying event planning for professionals.
+        </h1>
+        <p className='mt-5 max-w-prose text-zinc-700 sm:text-lg'>
+          Elevating events effortlessly, simplifying event planning for professionals, join us today to create remarkable moments together.
+        </p>
+
+        <Link
+          className={buttonVariants({
+            size: 'lg',
+            className: 'mt-5',
+          })}
+          href='/'
+          target='_blank'>
+          Get started{' '}
+          <ArrowRight className='ml-2 h-5 w-5' />
+        </Link>
+      </MaxWidthWrapper>
+    </>
   )
 }
